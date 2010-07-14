0365b387e2338c036f96c4cdbd645e28509bd996
ever.zet <ever.zet@gmail.com>
2010-07-14 14:37:42 +0300

    refactored features directory structure to reflect cucumber's one

---
+++ tests/Behat/CalculatorTest/features/steps/math_steps.php
@@ -0,0 +1,25 @@
+<?php
+
+$steps->Given('/I have entered (\d+) into the calculator/', function($num) use($world) {
+    $world->nums[] = $num;
+});
+
+$steps->When('/I press add/', function() use($world) {
+    $world->result = 0;
+    foreach ($world->nums as $num) {
+        $world->result += $num;
+    }
+    $world->nums = array();
+});
+
+$steps->When('/I press div/', function() use($world) {
+    $world->result = array_shift($world->nums);
+    foreach ($world->nums as $num) {
+        $world->result /= $num;
+    }
+    $world->nums = array();
+});
+
+$steps->Then('/the result should be (\d+) on the screen/', function($result) use($world) {
+    assertEquals($result, $world->result);
+});
---
+++ tests/Behat/WorldTest/features/steps/world_steps.php
@@ -0,0 +1,14 @@
+<?php
+
+$steps->Given('/I have entered (\d+)/', function($num) use($world) {
+    assertNull($world->value);
+    $world->value = $num;
+});
+
+$steps->Then('/I must have (\d+)/', function($num) use($world) {
+    assertEquals($num, $world->value);
+});
+
+$steps->When('/I add (\d+)/', function($num) use($world) {
+    $world->value += $num;
+});
