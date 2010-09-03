af58d7f1c101753f4d6022dba4caeab4937177b0
ever.zet <ever.zet@gmail.com>
2010-09-04 01:22:14 +0300

    removed unneded use

--- src/Everzet/Behat/Console/Command/TestCommand.php
+++ src/Everzet/Behat/Console/Command/TestCommand.php
@@ -5,7 +5,6 @@ namespace Everzet\Behat\Console\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Everzet\Behat\ServiceContainer\Container;
