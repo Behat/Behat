92a6a2dd2bde3f2b738c6a128eb746b850e6a0a1
ever.zet <ever.zet@gmail.com>
2010-09-07 01:04:26 +0300

    added some routines for better Behat Behat tests +)

--- tests/Behat/features/steps/behat_steps.php
+++ tests/Behat/features/steps/behat_steps.php
@@ -13,10 +13,11 @@ $steps->Given('/^a standard Behat project directory structure$/', function() use
 });
 
 $steps->Given('/^a file named "([^"]*)" with:$/', function($filename, $content) use($world) {
-    file_put_contents($filename, $content);
+    file_put_contents($filename, strtr($content, array("'''" => '"""')));
 });
 
 $steps->When('/^I run "([^"]*)"$/', function($command) use($world) {
+    $world->command = $command;
     exec($command, $world->output, $world->return);
 
     // Remove formatting & time from output
@@ -26,13 +27,11 @@ $steps->When('/^I run "([^"]*)"$/', function($command) use($world) {
 });
 
 $steps->Then('/^display last command exit code$/', function() use($world) {
-    echo "\n\n(==) debug (==)\n  exit:" . $world->return . "\n(==) debug (==)\n\n";
+    $world->printDebug("`" . $world->command . "`  =>  " . $world->return);
 });
 
 $steps->Then('/^display last command output$/', function() use($world) {
-    echo "\n\n(==) debug (==)\n  " .
-        strtr($world->output, array("\n" => "\n  ")) .
-        "\n(==) debug (==)\n\n";
+    $world->printDebug("`" . $world->command . "`:\n" . $world->output);
 });
 
 $steps->Then('/^it should (fail|pass) with:$/', function($success, $data) use($world) {
