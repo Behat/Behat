52cb9783819e73bcef1bf3e0292089973b088d6a
ever.zet <ever.zet@gmail.com>
2011-02-13 00:42:54 +0200

    optimized formatter for windows support

--- src/Behat/Behat/Formatter/ProgressFormatter.php
+++ src/Behat/Behat/Formatter/ProgressFormatter.php
@@ -351,7 +351,7 @@ class ProgressFormatter extends ConsoleFormatter
     protected function relativizePathsInString($string)
     {
         if (null !== ($basePath = $this->parameters->get('base_path'))) {
-            $string = str_replace(dirname($basePath) . '/', '', $string);
+            $string = str_replace(dirname($basePath) . DIRECTORY_SEPARATOR, '', $string);
         }
 
         return $string;
