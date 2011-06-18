2f976c277829ce5b51cb794bd15fd05661c87878
everzet <ever.zet@gmail.com>
2011-06-18 16:43:12 +0300

    refactored pear compilator

--- src/Behat/Behat/Compiler/PearCompiler.php
+++ src/Behat/Behat/Compiler/PearCompiler.php
@@ -1,3 +1,116 @@
+<?php
+
+namespace Behat\Behat\Compiler;
+
+use Symfony\Component\Finder\Finder;
+
+/*
+ * This file is part of the Behat.
+ * (c) Konstantin Kudryashov <ever.zet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Pear package compiler.
+ *
+ * @author      Konstantin Kudryashov <ever.zet@gmail.com>
+ */
+class PearCompiler
+{
+    /**
+     * Behat lib directory.
+     *
+     * @var     string
+     */
+    private $libPath;
+
+    /**
+     * Initializes compiler.
+     */
+    public function __construct()
+    {
+        $this->libPath = realpath(__DIR__ . '/../../../../');
+    }
+
+    /**
+     * Compiles pear package.
+     *
+     * @param   string  $version
+     */
+    public function compile($version, $stability)
+    {
+        if (file_exists('package.xml')) {
+            unlink('package.xml');
+        }
+        file_put_contents('package.xml', $this->getPackageTemplate());
+
+        $finder = new Finder();
+        $finder->files()
+            ->ignoreVCS(true)
+            ->name('*.php')
+            ->name('*.xsd')
+            ->name('*.xml')
+            ->name('*.xliff')
+            ->name('*.feature')
+            ->name('LICENSE')
+            ->notName('PharCompiler.php')
+            ->notName('PearCompiler.php')
+            ->in($this->libPath . '/src')
+            ->in($this->libPath . '/i18n')
+            ->in($this->libPath . '/vendor/Symfony')
+            ->in($this->libPath . '/features');
+
+        $xmlSourceFiles = '';
+        foreach ($finder as $file) {
+            $path = str_replace($this->libPath . '/', '', $file->getRealPath());
+            $xmlSourceFiles .=
+                '<file role="php" baseinstalldir="behat" install-as="'.$path.'" name="'.$path.'" />'."\n";
+        }
+
+        $this->replaceTokens('package.xml', '##', '##', array(
+            'BEHAT_VERSION' => $version,
+            'CURRENT_DATE'  => date('Y-m-d'),
+            'SOURCE_FILES'  => $xmlSourceFiles,
+            'STABILITY'     => $stability
+        ));
+
+        system('pear package');
+        unlink('package.xml');
+    }
+
+    /**
+     * Replaces tokens in specified path.
+     *
+     * @param   string|array    $files          files array or single file
+     * @param   string          $tokenStart     token start symbol
+     * @param   string          $tokenFinish    token finish symbol
+     * @param   array           $tokens         replace tokens array
+     */
+    protected function replaceTokens($files, $tokenStart, $tokenFinish, array $tokens)
+    {
+        if (!is_array($files)) {
+            $files = array($files);
+        }
+
+        foreach ($files as $file) {
+            $content = file_get_contents($file);
+            foreach ($tokens as $key => $value) {
+                $content = str_replace($tokenStart . $key . $tokenFinish, $value, $content, $count);
+            }
+            file_put_contents($file, $content);
+        }
+    }
+
+    /**
+     * Returns pear package template.
+     *
+     * @return  string
+     */
+    protected function getPackageTemplate()
+    {
+        return <<<'EOF'
 <?xml version="1.0" encoding="UTF-8"?>
 <package packagerversion="1.8.0" version="2.0" xmlns="http://pear.php.net/dtd/package-2.0" xmlns:tasks="http://pear.php.net/dtd/tasks-1.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://pear.php.net/dtd/tasks-1.0
     http://pear.php.net/dtd/tasks-1.0.xsd
@@ -18,7 +131,7 @@
     <date>##CURRENT_DATE##</date>
     <version>
         <release>##BEHAT_VERSION##</release>
-        <api>1.0.0</api>
+        <api>2.0.0</api>
     </version>
     <stability>
         <release>##STABILITY##</release>
@@ -28,14 +141,12 @@
     <notes>-</notes>
     <contents>
         <dir name="/">
-
             ##SOURCE_FILES##
 
             <file role="script" baseinstalldir="/" name="bin/behat.php">
                 <tasks:replace from="/usr/bin/env php" to="php_bin" type="pear-config" />
                 <tasks:replace from="DEV" to="version" type="package-info" />
             </file>
-
             <file role="script" baseinstalldir="/" name="bin/behat.bat">
                 <tasks:replace from="@php_bin@" to="php_bin" type="pear-config" />
                 <tasks:replace from="@bin_dir@" to="bin_dir" type="pear-config" />
@@ -43,11 +154,9 @@
 
             <file role="php" baseinstalldir="behat" name="autoload.php.dist" />
             <file role="php" baseinstalldir="behat" name="behat.yml" />
-
             <file role="php" baseinstalldir="behat" name="CHANGES.md" />
-            <file role="php" baseinstalldir="behat" name="LICENSE" />
             <file role="php" baseinstalldir="behat" name="README.md" />
-
+            <file role="php" baseinstalldir="behat" name="LICENSE" />
         </dir>
     </contents>
     <dependencies>
@@ -61,7 +170,7 @@
             <package>
                 <name>gherkin</name>
                 <channel>pear.behat.org</channel>
-                <min>1.0.5</min>
+                <min>1.0.6</min>
             </package>
             <extension>
                 <name>pcre</name>
@@ -95,3 +204,6 @@
         </filelist>
      </phprelease>
 </package>
+EOF;
+    }
+}
