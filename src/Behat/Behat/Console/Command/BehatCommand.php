08e79781e926613c1b73685393af660dfd579006
everzet <ever.zet@gmail.com>
2011-07-29 16:02:09 +0300

    update to the Symfony2 stable components

--- src/Behat/Behat/Console/Command/BehatCommand.php
+++ src/Behat/Behat/Console/Command/BehatCommand.php
@@ -9,8 +9,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder,
     Symfony\Component\Console\Input\InputOption,
     Symfony\Component\Console\Output\OutputInterface;
 
-use Behat\Behat\Console\Input\InputDefinition,
-    Behat\Behat\Console\Processor,
+use Behat\Behat\Console\Processor,
     Behat\Behat\Event\SuiteEvent;
 
 /*
@@ -44,7 +43,6 @@ class BehatCommand extends BaseCommand
 
         $this
             ->setName('behat')
-            ->setDefinition(new InputDefinition())
             ->setProcessors(array(
                 new Processor\ContainerProcessor(),
                 new Processor\LocatorProcessor(),
