962c971e0c8c8f87dadc95374848d9d37edce9bc
everzet <ever.zet@gmail.com>
2011-08-06 04:45:44 +0300

    minified amount of needed extension points

--- src/Behat/Behat/Context/ContextDispatcher.php
+++ src/Behat/Behat/Context/ContextDispatcher.php
@@ -92,6 +92,6 @@ class ContextDispatcher
             throw new \RuntimeException('Specify context class to use for ContextDispatcher');
         }
 
-        return new $this->contextClass($this->parameters);
+        return new $this->contextClass($this->getContextParameters());
     }
 }
