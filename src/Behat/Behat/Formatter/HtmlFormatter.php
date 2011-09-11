004dcc8f915dad0b35fb0bd1f6e26691c5cd8d61
everzet <ever.zet@gmail.com>
2011-09-11 15:42:10 +0300

    use google CDN for jquery

--- src/Behat/Behat/Formatter/HtmlFormatter.php
+++ src/Behat/Behat/Formatter/HtmlFormatter.php
@@ -778,7 +778,7 @@ HTML
     <div id="behat">
         {{content}}
     </div>
-    <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.2.min.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js"></script>
     <script type="text/javascript">
         $(document).ready(function(){
             $('#behat .feature h2').click(function(){
