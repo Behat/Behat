699ce9ed3a6e69ce5533b0a484a49aead9ff3406
everzet <ever.zet@gmail.com>
2011-12-21 18:55:10 +0100

    updated to support latest Symfony\Console changes

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -103,6 +103,11 @@ class FeatureContext extends BaseFeaturesContext
              : escapeshellarg(BEHAT_PHP_BIN_PATH);
         $argumentsString = strtr($argumentsString, array('\'' => '"'));
 
+        // close STDERR
+        if (is_resource(STDERR)) {
+            fclose(STDERR);
+        }
+
         exec($php . ' ' . escapeshellarg(BEHAT_BIN_PATH) . ' --lang=en --no-time --no-colors ' . $argumentsString, $output, $return);
 
         $this->command = 'behat ' . $argumentsString;
