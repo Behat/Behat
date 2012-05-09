4dc09eadcf37998467c448f1ed36c829bff6636f
everzet <ever.zet@gmail.com>
2012-05-09 23:29:50 +0200

    removed default value as it's not needed

--- src/Behat/Behat/Console/Processor/LocatorProcessor.php
+++ src/Behat/Behat/Console/Processor/LocatorProcessor.php
@@ -49,8 +49,7 @@ class LocatorProcessor extends Processor
             "- a feature <comment>(*.feature)</comment>\n" .
             "- a scenario at specific line <comment>(*.feature:10)</comment>.\n" .
             "- all scenarios at or after a specific line <comment>(*.feature:10-*)</comment>.\n" .
-            "- all scenarios at a line within a specific range <comment>(*.feature:10-20)</comment>.",
-            ''
+            "- all scenarios at a line within a specific range <comment>(*.feature:10-20)</comment>."
         );
     }
 
