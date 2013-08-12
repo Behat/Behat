bea8eb4321361697dab1cb60767c25751a5a8398
everzet <ever.zet@gmail.com>
2013-08-12 21:29:15 +0100

    Remove obsolete use statement

--- src/Behat/Behat/Suite/SuiteInterface.php
+++ src/Behat/Behat/Suite/SuiteInterface.php
@@ -9,7 +9,6 @@ namespace Behat\Behat\Suite;
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-use Behat\Gherkin\Filter\FilterInterface;
 
 /**
  * Basic suite.
