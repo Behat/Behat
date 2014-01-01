0159545c39aaea5946e6333a4687560170dcc068
everzet <ever.zet@gmail.com>
2014-01-01 22:32:30 +0000

    Bump application version

--- src/Behat/Behat/ApplicationFactory.php
+++ src/Behat/Behat/ApplicationFactory.php
@@ -39,7 +39,7 @@ use Behat\Testwork\Suite\ServiceContainer\SuiteExtension;
  */
 class ApplicationFactory extends BaseFactory
 {
-    const VERSION = '3.0-dev';
+    const VERSION = '3.0.0rc1';
 
     /**
      * Returns application name.
