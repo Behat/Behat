b0ba3f33e9ba52e03659736c9813d2c8ffa28198
Peter Rehm <peter.rehm@renvest.de>
2014-01-02 13:19:12 +0100

    Added german translation

--- i18n.php
+++ i18n.php
@@ -47,4 +47,20 @@
             'undefined_count'        => '{1} 1 non definito|]1,Inf] %1% non definiti',
             'skipped_count'          => '{1} 1 ignorato|]1,Inf] %1% ignorati',
         ),
+    'de' =>
+        array(
+            'snippet_proposal_title' => '<snippet_undefined><snippet_keyword>%1%</snippet_keyword> hat fehlende Schritte. Definiere diese mit den folgenden Snippets:</snippet_undefined>',
+            'snippet_missing_title'  => '<snippet_undefined>Snippets für die folgenden Schritte in der <snippet_keyword>%1%</snippet_keyword> Suite wurden nicht generiert (Überprüfen sie ihre Konfiguration):</snippet_undefined>',
+            'failed_scenarios_title' => 'Fehlgeschlagene Szenarien:',
+            'failed_hooks_title'     => 'Fehlgeschlagene Hooks:',
+            'failed_steps_title'     => 'Fehlgeschlagene Schritte:',
+            'pending_steps_title'    => 'Ausstehende Schritte:',
+            'scenarios_count'        => '{0} Kein Szenario|{1} 1 Szenario|]1,Inf] %1% Szenarien',
+            'steps_count'            => '{0} Kein Schritt|{1} 1 Schritt|]1,Inf] %1% Schritte',
+            'passed_count'           => '[1,Inf] %1% bestanden',
+            'failed_count'           => '[1,Inf] %1% fehlgeschlagen',
+            'pending_count'          => '[1,Inf] %1% ausstehend',
+            'undefined_count'        => '[1,Inf] %1% undefiniert',
+            'skipped_count'          => '[1,Inf] %1% übersprungen',
+        ),        
 );
