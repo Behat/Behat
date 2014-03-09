14c0fff5bfa9b65ee9cbbaa5a91e2df2962e451b
everzet <ever.zet@gmail.com>
2014-03-09 13:49:17 +0000

    Hook scopes were introduced as a way to decouple hooks from events

---
+++ src/Behat/Testwork/Hook/Scope/AfterTestScope.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Behat.
+ * (c) Konstantin Kudryashov <ever.zet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Behat\Testwork\Hook\Scope;
+
+use Behat\Testwork\Tester\Result\TestResult;
+
+/**
+ * Testwork after test scope interface.
+ *
+ * @author Konstantin Kudryashov <ever.zet@gmail.com>
+ */
+interface AfterTestScope extends HookScope
+{
+    /**
+     * Returns test result.
+     *
+     * @return TestResult
+     */
+    public function getTestResult();
+}
