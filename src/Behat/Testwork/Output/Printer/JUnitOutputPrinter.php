0819aa54fb7dc20a16edd210ab3f27464fb7450c
WouterJ <waldio.webdesign@gmail.com>
2014-03-20 15:23:02 +0100

    Renamed ConsoleOutputPrinter to StreamOutputPrinter
    The class is now stream-independent. The ConsoleOutputPrinter in
    Behat\Behat is kept, since it's created specifically for the console.

---
+++ src/Behat/Testwork/Output/Printer/JUnitOutputPrinter.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Behat Testwork.
+ * (c) Konstantin Kudryashov <ever.zet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Behat\Testwork\Output\Printer;
+
+use Behat\Testwork\Output\Exception\BadOutputPathException;
+
+/**
+ * A file compatible file output printer.
+ *
+ * @author Wouter J <wouter@wouterj.nl>
+ */
+class JUnitOutputPrinter extends StreamOutputPrinter
+{
+}
