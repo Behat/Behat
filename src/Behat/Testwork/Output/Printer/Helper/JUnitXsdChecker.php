78e86b893238a1063ae5215e39134bcb30e17d76
WouterJ <waldio.webdesign@gmail.com>
2014-03-21 09:43:30 +0100

    Wrote JUnitOutputPrinter

---
+++ src/Behat/Testwork/Output/Printer/Helper/JUnitXsdChecker.php
@@ -0,0 +1,120 @@
+<?php
+
+/*
+ * This file is part of the Behat Testwork.
+ * (c) Konstantin Kudryashov <ever.zet@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Behat\Testwork\Output\Printer\Helper;
+
+/**
+ * A class for checking if attributes or elements comply to the JUnit XSD.
+ *
+ * @author Wouter J <wouter@wouterj.nl>
+ */
+class JUnitXsdChecker
+{
+    /**
+     * Validates the attributes for the <testsuites> element.
+     *
+     * @param array $attributes
+     *
+     * @see checkAttributes
+     */
+    public function validateTestsuitesAttributes(array $attributes)
+    {
+        $this->checkAttributes($attributes, array('name', 'time', 'tests', 'failures', 'disabled', 'errors'), 'testsuites');
+    }
+
+    /**
+     * Validates the attributes for the <testsuite> element.
+     *
+     * @param array $attributes
+     *
+     * @see checkAttributes
+     */
+    public function validateTestsuitesAttributes(array $attributes)
+    {
+        $this->checkAttributes($attributes, array('name', 'tests', 'failures', 'errors', 'time', 'disabled', 'skipped', 'timestamp', 'hostname', 'id', 'package'), 'testsuite');
+    }
+
+    /**
+     * Validates the attributes for the <testcase> element.
+     *
+     * @param array $attributes
+     *
+     * @see checkAttributes
+     */
+    public function validateTestcaseAttributes(array $attributes)
+    {
+        $this->checkAttributes($attributes, array('name', 'assertions', 'time', 'classname', 'status'), 'testsuite');
+    }
+
+    /**
+     * Validates the children elements for the <testcase> element.
+     *
+     * @param array $nodeName
+     *
+     * @throws \Logicexception When the nodename is invalid
+     */
+    public function validateTestcaseChildNodeName($nodeName)
+    {
+        $validNodeNames = array('skipped', 'error', 'failure', 'system-out', 'system-err');
+        if (!in_array($nodeName, $validNodeNames)) {
+            throw new \LogicException(sprintf(
+                'Invalid node name (%s) given for a child of the <testcase> element, supported node names: %s',
+                $nodeName,
+                implode(', ', $validNodeNames)
+            ));
+        }
+    }
+
+    public function validateTestcaseChildAttributes($nodeName, array $nodeAttributes)
+    {
+        $supportedAttributes = array();
+        switch ($nodeName) {
+            case 'skipped':
+            case 'system-err':
+            case 'system-out':
+                break;
+
+            case 'error':
+            case 'failure':
+                $supportedAttributes = array('type', 'message');
+                break;
+        }
+
+        $this->checkAttributes($nodeAttributes, $supportedAttributes);
+    }
+
+    /**
+     * Checks if there are unsupported attributes.
+     *
+     * @param array  $actualAttributes
+     * @param array  $supportedAttributes
+     * @param string $elementName
+     *
+     * @throws \LogicException When there are invalid attributes
+     */
+    protected function checkAttributes(array $actualAttributes, array $supportedAttributes, $elementName)
+    {
+        $invalidAttributes = array();
+        foreach ($actualAttributes as $attributeName => $attributeValue) {
+            if (!in_array($attribute)) {
+                $invalidAttributes[] = $attributeName;
+            }
+        }
+
+        if (0 !== count($invalidAttributes)) {
+            throw new \LogicException(sprintf(
+                'Invalid attributes (%s) given for <%s> element, supported attributes: %s',
+                implode(', ', $invalidAttributes),
+                $elementName,
+                implode(', ', $supportedAttributes)
+            ));
+        }
+    }
+}
--- src/Behat/Testwork/Output/Printer/JUnitOutputPrinter.php
+++ src/Behat/Testwork/Output/Printer/JUnitOutputPrinter.php
@@ -10,13 +10,239 @@
 
 namespace Behat\Testwork\Output\Printer;
 
+use Behat\Testwork\Output\Printer\Helper\JUnitXsdChecker;
 use Behat\Testwork\Output\Exception\BadOutputPathException;
+use Symfony\Component\Console\Output\StreamOutput;
 
 /**
- * A file compatible file output printer.
+ * A convient wrapper around the StreamOutputPrinter to write valid JUnit 
+ * reports.
  *
  * @author Wouter J <wouter@wouterj.nl>
  */
 class JUnitOutputPrinter extends StreamOutputPrinter
 {
+    /**
+     * @var null|string
+     */
+    private $fileName;
+    /**
+     * @var JUnitXsdChecker
+     */
+    private $xsdChecker;
+    /**
+     * @var boolean
+     */
+    private $testsuitesNodeOpen = false;
+    /**
+     * @var boolean
+     */
+    private $testsuiteNodeOpen = false;
+    /**
+     * @var boolean
+     */
+    private $testcaseNodeOpen = false;
+    /**
+     * @var boolean
+     */
+    private $testcaseNodeHasChildren = false;
+
+    public function __construct(JUnitXsdChecker $xsdChecker)
+    {
+        $this->xsdChecker = $xsdChecker;
+    }
+
+    /**
+     * Creates a new JUnit file.
+     *
+     * The file will be initialized with an XML definition and the root element.
+     *
+     * @param string $name                 The filename (without extension) and default value of the name attribute
+     * @param array  $testsuitesAttributes Attributes for the root element
+     */
+    public function createNewFile($name, array $testsuitesAttributes = array())
+    {
+        $this->flush();
+
+        $this->setFileName(trim(preg_replace('/[^[:alnum:]]+/', '-', $name), '-'));
+
+        $this->xsdChecker->validateTestsuitesAttributes($testsuitesAttributes);
+
+        $this->getWritingStream()->writeln(array(
+            '<?xml version="1.0" encoding="UTF-8" ?>',
+            sprintf(
+                '<testsuites%s>',
+                $this->printAttributes(array_merge(array('name' => $name), $testsuitesAttributes))
+            ),
+        ));
+
+        $this->testsuitesNodeOpen = true;
+    }
+
+    public function closeOpenTestsuites()
+    {
+        if ($this->testsuitesNodeOpen) {
+            $this->getWritingStream()->writeln('</testsuites>');
+        }
+    }
+
+    /**
+     * Adds a new <testsuite> node.
+     *
+     * @param array $testsuiteAttributes
+     */
+    public function addTestsuite(array $testsuiteAttributes = array())
+    {
+        $this->closeOpenTestsuite();
+
+        $this->xsdChecker->validateTestsuiteAttributes($testsuiteAttributes);
+
+        $this->getWritingStream()->writeln(sprintf(
+            '    <testsuite%s>',
+            $this->printAttributes($testsuiteAttributes)
+        ));
+
+        $this->testsuiteNodeOpen = true;
+    }
+
+    public function closeOpenTestsuite()
+    {
+        if ($this->testsuiteNodeOpen) {
+            $this->getWritingStream()->writeln('    </testsuite>');
+        }
+    }
+
+    /**
+     * Adds a new <testcase> node.
+     *
+     * @param array $testcaseAttributes
+     */
+    public function addTestcase(array $testcaseAttributes = array())
+    {
+        $this->closeOpenTestcase();
+
+        $this->xsdChecker->validateTestcaseAttributes($testcaseAttributes);
+
+        // no new line, since it may have no content
+        $this->getWritingStream()->write(sprintf(
+            '        <testcase%s',
+            $this->printAttributes($testcaseAttributes)
+        ));
+
+        $this->testcaseNodeHasChildren = false;
+        $this->testcaseNodeOpen = true;
+    }
+
+    public function closeOpenTestcase()
+    {
+        if ($this->testcaseNodeOpen) {
+            $this->getWritingStream()->writeln('        </testcase>');
+        }
+    }
+
+    /**
+     * Add a testcase child element.
+     *
+     * @param string $nodeName
+     * @param array  $nodeAttributes
+     * @param string $nodeValue
+     */
+    public function addTestcaseChild($nodeName, array $nodeAttributes = array(), $nodeValue = null)
+    {
+        $this->validateTestcaseChildNodeName($nodeName);
+        $this->validateTestcaseChildAttributes($nodeName, $nodeAttributes);
+
+        $this->getWritingStream()->writeln(array(
+            '>', // close testcase open-tag
+            sprintf(
+                '<%s%s%s%s%s',
+                $nodeName,
+                $this->printAttributes($nodeAttributes),
+                null === $nodeValue ? '/>' : '>',
+                $nodeValue,
+                null === $nodeValue ? '</'.$nodeName.'>' : null
+            ),
+        ));
+
+        $this->testcaseNodeHasChildren = true;
+    }
+
+    /**
+     * Sets file name.
+     *
+     * @param string $fileName
+     * @param string $extension The file extension, defaults to "xml"
+     */
+    public function setFileName($fileName, $extension = 'xml')
+    {
+        if ('.'.$extension !== substr($fileName, strlen($extension) + 1)) {
+            $fileName .= '.'.$extension;
+        }
+
+        $this->fileName = $fileName;
+    }
+
+    public function closeAllOpenElements()
+    {
+        $this->closeOpenTestcase();
+        $this->closeOpenTestsuite();
+        $this->closeOpenTestsuites();
+    }
+
+    /**
+     * Prints the attributes in a XML valid way.
+     *
+     * @param array   $attributes
+     * @param boolean $prefixWithSpace
+     *
+     * @return string
+     */
+    protected function printAttributes(array $attributes, $prefixWithSpace = true)
+    {
+        return ($prefixWithSpace ? ' ' : '') . implode(' ', array_walk($attributes, function ($value, $name) {
+            return $name . '=' . $value;
+        }));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function createOutputStream()
+    {
+        if (!is_dir($this->outputPath)) {
+            throw new BadOutputPathException(sprintf(
+                'Directory expected for the `output_path` option, given `%s`.',
+                $this->outputPath
+            ), $this->outputPath);
+        }
+
+        if (null === $this->fileName) {
+            throw new 
+        }
+
+        $filePath = $this->outputPath.'/'.$this->fileName;
+
+        $stream = new StreamOutput(
+            fopen($filePath, 'w'),
+            StreamOutput::VERBOSITY_NORMAL,
+            false // a file is never decorated
+        );
+        $this->configureOutputStream($stream);
+
+        return $stream;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function flush()
+    {
+        $this->closeAllOpenElements();
+        parent::flush();
+
+        $this->testsuitesNodeOpen = false;
+        $this->testsuiteNodeOpen = false;
+        $this->testcaseNodeOpen = false;
+        $this->testcaseNodeHasChildren = false;
+    }
 }
