357590999f406c1b8ee93c8127460e7e1632d7e2
everzet <ever.zet@gmail.com>
2014-06-29 15:52:37 +0100

    Fix bug and conduct refactoring in the parameter generation

--- src/Behat/Behat/Definition/Search/RepositorySearchEngine.php
+++ src/Behat/Behat/Definition/Search/RepositorySearchEngine.php
@@ -20,6 +20,7 @@ use Behat\Gherkin\Node\ArgumentInterface;
 use Behat\Gherkin\Node\FeatureNode;
 use Behat\Gherkin\Node\StepNode;
 use Behat\Testwork\Environment\Environment;
+use ReflectionParameter;
 use Symfony\Component\Translation\TranslatorInterface;
 
 /**
@@ -122,48 +123,138 @@ final class RepositorySearchEngine implements SearchEngine
      * @param string[]            $match
      * @param ArgumentInterface[] $multiline
      *
-     * @return array
+     * @return mixed[]
+     *
+     * @throws UnknownParameterValueException
      */
     private function getArguments(Definition $definition, array $match, array $multiline)
     {
-        reset($multiline);
-        $namedMatches = count($match) > count(array_filter(array_keys($match), 'is_numeric'));
+        $parameters = $definition->getReflection()->getParameters();
 
+        $arguments = $this->getMatchedArguments($match, $parameters);
+        $arguments = $this->appendMultilineArguments($multiline, $parameters, $arguments);
+
+        $this->validateArguments($definition, $parameters, $arguments);
+
+        return $arguments;
+    }
+
+    /**
+     * Returns reflection parameter names.
+     *
+     * @param ReflectionParameter[] $parameters
+     *
+     * @return string[]
+     */
+    private function getParameterNames(array $parameters)
+    {
+        return array_map(
+            function (ReflectionParameter $parameter) {
+                return $parameter->getName();
+            }, $parameters
+        );
+    }
+
+    /**
+     * Returns array of matched arguments.
+     *
+     * @param array                 $match
+     * @param ReflectionParameter[] $parameters
+     *
+     * @return mixed[]
+     */
+    private function getMatchedArguments(array $match, array $parameters)
+    {
         $arguments = array();
-        foreach ($definition->getReflection()->getParameters() as $num => $parameter) {
-            $class = $parameter->getClass();
-            if (null !== $class && $class->implementsInterface('Behat\Gherkin\Node\ArgumentInterface')) {
-                $arguments[] = current($multiline);
-                next($multiline);
 
-                continue;
-            }
+        $names = $this->getParameterNames($parameters);
+        list($numArguments, $nameArguments) = $this->splitNumberedAndNamedArguments($match, $names);
 
-            if ($namedMatches) {
-                if (isset($match[$parameter->getName()])) {
-                    $arguments[$parameter->getName()] = $match[$parameter->getName()];
-                } elseif ($parameter->isDefaultValueAvailable()) {
-                    $arguments[$parameter->getName()] = $parameter->getDefaultValue();
-                } else {
-                    throw new UnknownParameterValueException(sprintf(
-                        'The value of parameter `%s` of method `%s#%s` cannot be determined.',
-                        $parameter->getName(),
-                        $definition->getReflection()->getDeclaringClass()->getName(),
-                        $definition->getReflection()->getName()
-                    ));
-                }
-            } elseif (isset($match[$num])) {
-                $arguments[$num] = $match[$num];
-            } else {
-                throw new UnknownParameterValueException(sprintf(
-                    'The value of parameter `%s` of method `%s#%s` cannot be determined.',
-                    $parameter->getName(),
-                    $definition->getReflection()->getDeclaringClass()->getName(),
-                    $definition->getReflection()->getName()
-                ));
+        foreach ($parameters as $num => $parameter) {
+            $name = $parameter->getName();
+
+            if (isset($nameArguments[$name])) {
+                $arguments[$name] = $nameArguments[$name];
+            } elseif (isset($numArguments[$num])) {
+                $arguments[$num] = $numArguments[$num];
+            } elseif ($parameter->isDefaultValueAvailable()) {
+                $arguments[$num] = $parameter->getDefaultValue();
             }
         }
 
         return $arguments;
+    }
+
+    /**
+     * Appends multiline arguments to the end of the arguments list.
+     *
+     * @param ArgumentInterface[]   $multiline
+     * @param ReflectionParameter[] $parameters
+     * @param mixed[]               $arguments
+     *
+     * @return mixed[]
+     */
+    private function appendMultilineArguments(array $multiline, array $parameters, array $arguments)
+    {
+        foreach (array_values($multiline) as $num => $argument) {
+            $arguments[count($parameters) - 1 - $num] = $argument;
+        }
+
+        return $arguments;
+    }
+
+    /**
+     * Validates that all arguments are in place, throws exception otherwise.
+     *
+     * @param Definition            $definition
+     * @param ReflectionParameter[] $parameters
+     * @param mixed[]               $arguments
+     *
+     * @throws UnknownParameterValueException
+     */
+    private function validateArguments(Definition $definition, array $parameters, array $arguments)
+    {
+        foreach ($parameters as $num => $parameter) {
+            $name = $parameter->getName();
+
+            if (isset($arguments[$num]) || isset($arguments[$name])) {
+                continue;
+            }
+
+            throw new UnknownParameterValueException(sprintf(
+                'Can not find a matching value for an argument `%s` of the method `%s`.',
+                $name,
+                $definition->getPath()
+            ));
+        }
+    }
+
+    /**
+     * Splits matches into two separate arrays - numbered and named.
+     *
+     * `preg_match` matches named arguments with named indexes and also
+     * represents all arguments with numbered indexes. This method splits
+     * that one array into two independent ones.
+     *
+     * @param array $match
+     * @param array $parameterNames
+     *
+     * @return array
+     */
+    private function splitNumberedAndNamedArguments(array $match, array $parameterNames)
+    {
+        $numberedArguments = $match;
+        $namedArguments = array();
+
+        foreach ($match as $key => $val) {
+            if (!is_integer($key) && in_array($key, $parameterNames)) {
+                $namedArguments[$key] = $val;
+
+                unset($numberedArguments[$key]);
+                unset($numberedArguments[array_search($val, $numberedArguments)]);
+            }
+        }
+
+        return array($numberedArguments, $namedArguments);
     }
 }
