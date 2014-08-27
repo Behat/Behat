3f4f68d6ce0cb1e6615b41d70c1f8d25a28f4150
James <james@sitepulse.org>
2014-08-27 12:49:13 +0100

    Reverted back to original state

--- src/Behat/Behat/Output/Node/EventListener/AST/SuiteListener.php
+++ src/Behat/Behat/Output/Node/EventListener/AST/SuiteListener.php
@@ -11,10 +11,8 @@
 namespace Behat\Behat\Output\Node\EventListener\AST;
 
 use Behat\Behat\Output\Node\Printer\SetupPrinter;
-use Behat\Behat\Output\Node\Printer\SuitePrinter;
 use Behat\Testwork\EventDispatcher\Event\AfterSuiteSetup;
 use Behat\Testwork\EventDispatcher\Event\AfterSuiteTested;
-use Behat\Testwork\EventDispatcher\Event\BeforeSuiteTested;
 use Behat\Testwork\Output\Formatter;
 use Behat\Testwork\Output\Node\EventListener\EventListener;
 use Symfony\Component\EventDispatcher\Event;
@@ -30,21 +28,15 @@ final class SuiteListener implements EventListener
      * @var SetupPrinter
      */
     private $setupPrinter;
-    /**
-     * @var null|SuitePrinter
-     */
-    private $suitePrinter;
 
     /**
      * Initializes listener.
      *
      * @param SetupPrinter $setupPrinter
-     * @param SuitePrinter $suitePrinter
      */
-    public function __construct(SetupPrinter $setupPrinter = null, SuitePrinter $suitePrinter = null)
+    public function __construct(SetupPrinter $setupPrinter)
     {
         $this->setupPrinter = $setupPrinter;
-        $this->suitePrinter = $suitePrinter;
     }
 
     /**
@@ -52,22 +44,12 @@ final class SuiteListener implements EventListener
      */
     public function listenEvent(Formatter $formatter, Event $event, $eventName)
     {
-        if ($event instanceof AfterSuiteSetup && $this->setupPrinter) {
+        if ($event instanceof AfterSuiteSetup) {
             $this->setupPrinter->printSetup($formatter, $event->getSetup());
         }
 
-        if ($event instanceof BeforeSuiteTested && $this->suitePrinter) {
-            $this->suitePrinter->printHeader($formatter, $event->getSuite());
-        }
-
         if ($event instanceof AfterSuiteTested) {
-            if ($this->setupPrinter) {
-                $this->setupPrinter->printTeardown($formatter, $event->getTeardown());
-            }
-
-            if ($this->suitePrinter) {
-                $this->suitePrinter->printFooter($formatter, $event->getSuite());
-            }
+            $this->setupPrinter->printTeardown($formatter, $event->getTeardown());
         }
     }
 }
