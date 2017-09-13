8ab237fe7ca886a909b81830ea309fe9de996914
Scott Moorhouse <Scott.Moorhouse@allegiantair.com>
2017-09-13 11:37:41 -0700

    Removing short array syntax

--- src/Behat/Behat/Gherkin/ServiceContainer/GherkinExtension.php
+++ src/Behat/Behat/Gherkin/ServiceContainer/GherkinExtension.php
@@ -225,7 +225,7 @@ final class GherkinExtension implements Extension
         }
 
         $definition->addMethodCall('setCache', array($cacheDefinition));
-        $definition->addMethodCall('setBasePath', ['%paths.base%']);
+        $definition->addMethodCall('setBasePath', array('%paths.base%'));
         $definition->addTag(self::LOADER_TAG, array('priority' => 50));
         $container->setDefinition('gherkin.loader.gherkin_file', $definition);
     }
