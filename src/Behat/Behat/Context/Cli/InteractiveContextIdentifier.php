7581001fb9426e763e4f018395ee33af8e70ce01
Grégoire Paris <postmaster@greg0ire.fr>
2018-05-21 19:06:58 +0200

    Use a valid key as the default
    This is how the QuestionHelper API was intended to be used, but it only
    has been so since a recent bugfix.
    See https://github.com/symfony/symfony/pull/25521
    Fixes #1151

--- src/Behat/Behat/Context/Cli/InteractiveContextIdentifier.php
+++ src/Behat/Behat/Context/Cli/InteractiveContextIdentifier.php
@@ -70,7 +70,7 @@ final class InteractiveContextIdentifier implements TargetContextIdentifier
 
         $message = $this->translator->trans('snippet_context_choice', array('%1%' => $suiteName), 'output');
         $choices = array_values(array_merge(array('None'), $contextClasses));
-        $default = current($contextClasses);
+        $default = 1;
 
         $answer = $this->askQuestion('>> ' . $message, $choices, $default);
 
