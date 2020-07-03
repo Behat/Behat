3a74eceeaee06ab5f294123e9689193f3ef6792a
Remon van de Kamp <rpkamp@gmail.com>
2020-07-03 15:51:54 +0200

    Remove BC Layer for Symfony Translator versions 3 and lower

--- src/Behat/Behat/Definition/Translator/Translator.php
+++ src/Behat/Behat/Definition/Translator/Translator.php
@@ -4,12 +4,4 @@ namespace Behat\Behat\Definition\Translator;
 
 class Translator extends \Symfony\Component\Translation\Translator implements TranslatorInterface
 {
-    
-    public function trans($id, array $parameters = array(), $domain = null, $locale = null ) {
-        
-        if(array_key_exists('%count%', $parameters) && method_exists($this, 'transChoice') && !$this instanceof \Symfony\Contracts\Translation\TranslatorInterface){
-            return parent::transChoice($id, $parameters['%count%'], $parameters, $domain, $locale);
-        }
-        return parent::trans($id, $parameters, $domain, $locale);
-    }
 }
