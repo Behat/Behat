dd81416f1921c9a2c1635d40ddf9af94fed6c7cb
Pascal Paulis <ppaulis@gmail.com>
2022-04-06 08:37:09 +0200

    added file attribute to JUnit testcase-tag
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -312,7 +312,10 @@ EOL;
 
         $fileContent = trim(file_get_contents($path));
 
-        $fileContent = preg_replace('/time="(.*)"/', 'time="-IGNORE-VALUE-"', $fileContent);
+        $fileContent = preg_replace('/time="(.*)"/U', 'time="-IGNORE-VALUE-"', $fileContent);
+
+        // The placeholder is necessary because of different separators on Unix and Windows environments
+        $text = str_replace('-DIRECTORY-SEPARATOR-', DIRECTORY_SEPARATOR, $text);
 
         $dom = new DOMDocument();
         $dom->loadXML($text);
