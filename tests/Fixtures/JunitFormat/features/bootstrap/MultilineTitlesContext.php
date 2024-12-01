9b78c1749f32e64dd4c8504a173b47b3b60159b2
Carlos Granados <barel.barelon@gmail.com>
2024-12-01 20:45:35 +0100

    Use attributes in fixture contexts

--- tests/Fixtures/JunitFormat/features/bootstrap/MultilineTitlesContext.php
+++ tests/Fixtures/JunitFormat/features/bootstrap/MultilineTitlesContext.php
@@ -1,28 +1,25 @@
 <?php
 
 use Behat\Behat\Context\Context;
+use Behat\Step\Given;
+use Behat\Step\Then;
+use Behat\Step\When;
 
 class MultilineTitlesContext implements Context
 {
     private $value;
 
-    /**
-     * @Given /I have entered (\d+)/
-     */
+    #[Given('/I have entered (\d+)/')]
     public function iHaveEntered($num) {
         $this->value = $num;
     }
 
-    /**
-     * @Then /I must have (\d+)/
-     */
+    #[Then('/I must have (\d+)/')]
     public function iMustHave($num) {
         PHPUnit\Framework\Assert::assertEquals($num, $this->value);
     }
 
-    /**
-     * @When /I (add|subtract) the value (\d+)/
-     */
+    #[When('/I (add|subtract) the value (\d+)/')]
     public function iAddOrSubstact($op, $num) {
         if ($op == 'add')
             $this->value += $num;
--- tests/Fixtures/JunitFormat/features/bootstrap/StopOnFailureContext.php
+++ tests/Fixtures/JunitFormat/features/bootstrap/StopOnFailureContext.php
@@ -1,28 +1,25 @@
 <?php
 
 use Behat\Behat\Context\Context;
+use Behat\Step\Given;
+use Behat\Step\Then;
+use Behat\Step\When;
 
 class StopOnFailureContext implements Context
 {
     private $value;
 
-    /**
-     * @Given /I have entered (\d+)/
-     */
+    #[Given('/I have entered (\d+)/')]
     public function iHaveEntered($num) {
         $this->value = $num;
     }
 
-    /**
-     * @Then /I must have (\d+)/
-     */
+    #[Then('/I must have (\d+)/')]
     public function iMustHave($num) {
         PHPUnit\Framework\Assert::assertEquals($num, $this->value);
     }
 
-    /**
-     * @When /I add (\d+)/
-     */
+    #[When('/I add (\d+)/')]
     public function iAdd($num) {
         $this->value += $num;
     }
