c235c8d28f281ab0d792eade4176fa92325208cf
Carlos Granados <barel.barelon@gmail.com>
2025-06-20 12:50:44 +0200

    Apply SimplifyIfReturnBoolRector rule

--- src/Behat/Testwork/Output/Cli/OutputController.php
+++ src/Behat/Testwork/Output/Cli/OutputController.php
@@ -189,18 +189,13 @@ class OutputController implements Controller
      */
     private function isAbsolutePath($file)
     {
-        if ($file[0] == '/' || $file[0] == '\\'
+        return $file[0] == '/' || $file[0] == '\\'
             || (
                 strlen($file) > 3 && ctype_alpha($file[0])
                 && $file[1] == ':'
                 && ($file[2] == '\\' || $file[2] == '/')
             )
-            || null !== parse_url($file, PHP_URL_SCHEME)
-        ) {
-            return true;
-        }
-
-        return false;
+            || null !== parse_url($file, PHP_URL_SCHEME);
     }
 
     /**
