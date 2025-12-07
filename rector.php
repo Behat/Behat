5f164e7b562fbb2c5e16e88ee22711b72b574f03
acoulton <andrew@ingenerator.com>
2025-12-07 21:50:54 +0000

    refactor: Prep for incremental type bumping

--- rector.php
+++ rector.php
@@ -13,6 +13,7 @@ return RectorConfig::configure()
     ->withRootFiles()
     ->withPreparedSets(codeQuality: true)
     ->withPhpSets(php81: true)
+    ->withTypeCoverageLevel(0)
     ->withSkip([
         StringableForToStringRector::class,
     ])
