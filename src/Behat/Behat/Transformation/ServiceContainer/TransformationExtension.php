637c477ce99a340b66274a953c6f3dbb3c265c60
Alessandro Lai <alessandro.lai85@gmail.com>
2026-02-07 18:54:34 +0100

    Fix CompilerPassInterface::process implementations by adding the return type

--- src/Behat/Behat/Transformation/ServiceContainer/TransformationExtension.php
+++ src/Behat/Behat/Transformation/ServiceContainer/TransformationExtension.php
@@ -79,7 +79,7 @@ class TransformationExtension implements Extension
         $this->loadRepository($container);
     }
 
-    public function process(ContainerBuilder $container)
+    public function process(ContainerBuilder $container): void
     {
         $this->processArgumentsTransformers($container);
     }
