1b8396939f35b9f16f2ef5e95763f477848df23f
acoulton <andrew@ingenerator.com>
2026-04-17 21:58:52 +0100

    tmp: Reinstate our normal Rector rules

--- rector.php
+++ rector.php
@@ -1,8 +1,6 @@
 <?php
 
 declare(strict_types=1);
-use Ingenerator\RiskyRectorRules\PhpDocToStrictTypes\AddParamTypeFromPhpDocRector;
-use Ingenerator\RiskyRectorRules\PhpDocToStrictTypes\AddReturnTypeFromPhpDocRector;
 use Rector\Config\RectorConfig;
 use Rector\Php80\Rector\Class_\StringableForToStringRector;
 use Rector\Php81\Rector\Array_\ArrayToFirstClassCallableRector;
@@ -14,17 +12,13 @@ return RectorConfig::configure()
         __DIR__ . '/src',
     ])
     ->withRootFiles()
-//    ->withPreparedSets(codeQuality: true)
-//    ->withPhpSets(php82: true)
+    ->withPreparedSets(codeQuality: true)
+    ->withPhpSets(php82: true)
     ->withSkip([
         StringableForToStringRector::class,
         ReadOnlyClassRector::class,
         // DI setFactory does not support closures
         ArrayToFirstClassCallableRector::class => [__DIR__ . '/src/Behat/Testwork/Deprecation/ServiceContainer/DeprecationExtension.php'],
-    ])
-    ->withRules([
-        AddParamTypeFromPhpDocRector::class,
-        AddReturnTypeFromPhpDocRector::class,
     ])
     ->withImportNames(
         removeUnusedImports: true,
