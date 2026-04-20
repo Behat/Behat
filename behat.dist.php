3640cc16ab2a4442f2a2696f17ef9d75f640b802
acoulton <andrew@ingenerator.com>
2026-04-20 01:14:48 +0100

    ci: Fail on Behat deprecations in CI
    We should not be using any deprecated code within Behat itself.
    
    Obviously we might do inside our own features (e.g. to prove that
    deprecations are reported) but these are controlled by their own Behat
    config within the test fixture.
    
    Therefore, the outer Behat process that runs the features can safely
    fail on any Behat deprecation.

--- behat.dist.php
+++ behat.dist.php
@@ -3,9 +3,15 @@
 use Behat\Config\Config;
 use Behat\Config\GherkinOptions;
 use Behat\Config\Profile;
+use Behat\Config\TesterOptions;
 use Behat\Gherkin\GherkinCompatibilityMode;
 
 return (new Config())
     ->withProfile((new Profile('default'))
-        ->withGherkinOptions((new GherkinOptions())
-            ->withCompatibilityMode(GherkinCompatibilityMode::GHERKIN_32)));
+            ->withGherkinOptions((new GherkinOptions())
+                    ->withCompatibilityMode(GherkinCompatibilityMode::GHERKIN_32),
+            )
+        ->withTesterOptions((new TesterOptions())
+            ->withFailOnBehatDeprecations()
+        )
+    );
